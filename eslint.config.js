55c29fb12d6a6c2ad2157fc005ae7669a3341716
alex <sasha12887@gmail.com>
2024-11-19 04:06:50 +0200

    initial commit

--- eslint.config.js
+++ eslint.config.js
@@ -1,45 +1,31 @@
-// import globals from 'globals';
-// import tseslint from '@typescript-eslint/eslint-plugin';
-// import tseslintParser from '@typescript-eslint/parser';
-// import pluginReact from 'eslint-plugin-react';
-// import prettier from 'eslint-config-prettier';
-
-// /** @type {import('eslint').Linter.Config[]} */
-// export default [
-//   {
-//     files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
-//     languageOptions: {
-//       parser: tseslintParser,
-//       globals: globals.browser,
-//     },
-//     plugins: {
-//       '@typescript-eslint': tseslint,
-//       react: pluginReact,
-//     },
-//     settings: {
-//       react: {
-//         version: 'detect',
-//       },
-//     },
-//     rules: {
-//       ...tseslint.configs.recommended.rules,
-//       ...pluginReact.configs.recommended.rules,
-//       'react/react-in-jsx-scope': 'off',
-//     },
-//   },
-//   prettier,
-// ];
-
 import globals from 'globals';
-import tseslint from 'typescript-eslint';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tseslintParser from '@typescript-eslint/parser';
 import pluginReact from 'eslint-plugin-react';
 import prettier from 'eslint-config-prettier';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
-  { languageOptions: { globals: globals.browser } },
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    languageOptions: {
+      parser: tseslintParser,
+      globals: globals.browser,
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+      react: pluginReact,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      ...pluginReact.configs.recommended.rules,
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
   prettier,
 ];
