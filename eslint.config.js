9cd2918bea9c6f937fe1bb5548a8a056b1bd67b9
alex <sasha12887@gmail.com>
2024-11-19 03:54:58 +0200

    initial commit

---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+import globals from 'globals';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tseslintParser from '@typescript-eslint/parser';
+import pluginReact from 'eslint-plugin-react';
+import prettier from 'eslint-config-prettier';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    languageOptions: {
+      parser: tseslintParser,
+      globals: globals.browser,
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+      react: pluginReact,
+    },
+    settings: {
+      react: {
+        version: 'detect', // Automatically detects the React version
+      },
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      ...pluginReact.configs.recommended.rules,
+      'react/react-in-jsx-scope': 'off', // Disables the rule
+    },
+  },
+  prettier, // Prettier config
+];
