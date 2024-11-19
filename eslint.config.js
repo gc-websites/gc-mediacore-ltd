bd4c186fce7dcd641697e3e23f32483a4c3f81e5
alex <sasha12887@gmail.com>
2024-11-19 04:00:11 +0200

    initial commit

--- eslint.config.js
+++ eslint.config.js
@@ -1,31 +1,45 @@
+// import globals from 'globals';
+// import tseslint from '@typescript-eslint/eslint-plugin';
+// import tseslintParser from '@typescript-eslint/parser';
+// import pluginReact from 'eslint-plugin-react';
+// import prettier from 'eslint-config-prettier';
+
+// /** @type {import('eslint').Linter.Config[]} */
+// export default [
+//   {
+//     files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+//     languageOptions: {
+//       parser: tseslintParser,
+//       globals: globals.browser,
+//     },
+//     plugins: {
+//       '@typescript-eslint': tseslint,
+//       react: pluginReact,
+//     },
+//     settings: {
+//       react: {
+//         version: 'detect',
+//       },
+//     },
+//     rules: {
+//       ...tseslint.configs.recommended.rules,
+//       ...pluginReact.configs.recommended.rules,
+//       'react/react-in-jsx-scope': 'off',
+//     },
+//   },
+//   prettier,
+// ];
+
 import globals from 'globals';
-import tseslint from '@typescript-eslint/eslint-plugin';
-import tseslintParser from '@typescript-eslint/parser';
+import tseslint from 'typescript-eslint';
 import pluginReact from 'eslint-plugin-react';
 import prettier from 'eslint-config-prettier';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {
-    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
-    languageOptions: {
-      parser: tseslintParser,
-      globals: globals.browser,
-    },
-    plugins: {
-      '@typescript-eslint': tseslint,
-      react: pluginReact,
-    },
-    settings: {
-      react: {
-        version: 'detect',
-      },
-    },
-    rules: {
-      ...tseslint.configs.recommended.rules,
-      ...pluginReact.configs.recommended.rules,
-      'react/react-in-jsx-scope': 'off',
-    },
-  },
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { globals: globals.browser } },
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
   prettier,
 ];
